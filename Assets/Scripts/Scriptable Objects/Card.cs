62b8163a82501e01efa1bd4b60085fb11835ae22
Yu Ma <mayumayao@gmail.com>
2026-01-31 19:22:00 -0500

    somehow lose the playcard method

--- Assets/Scripts/Scriptable Objects/Card.cs
+++ Assets/Scripts/Scriptable Objects/Card.cs
@@ -1,58 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public enum CardType
-{
-    Attack,
-    Defense,
-    Power
-}
-public enum CardEffect
-{
-    Null,
-    DrunkenFist,
-}
-//[CreateAssetMenu(fileName = "Card", menuName = "ScriptableObjects/Card", order = 1)]
+public enum CardType { Attack, Defense, Power }
+public enum CardEffect { Null, DrunkenFist }
 
+[System.Serializable]
 public class Card
 {
-    //change this accordingly
-    
     public int ID;
     public Mood moodType;
     public string cardName;
     public CardType cardType;
     public CardEffect cardEffect;
 
-    // define your card properties here
-    public int damage;
-    public int shield;
-    public Card()
-    {
-        damage = 5;
-        cardType = CardType.Attack;
-    }
-    //example I will try something - Ricky
-    // can use abstract or virtual. I will use virtual for now
-    // see RedMask.cs for example of override (i just made this. change it however you want)
-    void resolveEffect()
+    public int damage = 5;
+    public int shield = 0;
+
+    public Card() { }
+
+    private void ResolveEffect()
     {
         switch (cardEffect)
         {
-            case (CardEffect.DrunkenFist):
+            case CardEffect.DrunkenFist:
                 int rng = Random.Range(0, 2);
-                if (rng == 0) damage = 0;
-                else damage = 12;
+                damage = (rng == 0) ? 0 : 12;
                 break;
-
         }
-
     }
+
     public virtual void UseCard()
     {
-        resolveEffect();
+        ResolveEffect();
         Debug.Log($"Using card: {cardName} with ID: {ID}");
-        // Implement card effect logic here
     }
 }
